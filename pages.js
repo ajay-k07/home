88f1094ee6fd01eb85705c07e39ee24046abf9ef
ajay <ajaidonglee@gmail.com>
2020-08-18 00:53:04 +0530

    resume update

--- pages.js
+++ pages.js
@@ -1,15 +1,15 @@
-// const ghpages = require("gh-pages");
-// const pathname = `${__dirname}/build`;
-// const repoURL = "https://github.com/ajailee/home.git";
+ const ghpages = require("gh-pages");
+ const pathname = `${__dirname}/build`;
+ const repoURL = "https://github.com/ajailee/home.git";
 
-// ghpages.publish(
-//   pathname,
-//   {
-//     branch: "master",
-//     repo: repoURL,
-//   },
-//   (err) => {
-//     if (err) console.log("ERROR: ", err);
-//     else console.log("PUBLISHED");
-//   }
-// );
+ ghpages.publish(
+   pathname,
+   {
+     branch: "master",
+     repo: repoURL,
+  },
+   (err) => {
+     if (err) console.log("ERROR: ", err);
+     else console.log("PUBLISHED");
+   }
+ );
